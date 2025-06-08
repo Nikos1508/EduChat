7dafe76dadd791fdefafcf5080b7dd07c66cdbc4
Nikos Tsilas <tsilasnikos@gmail.com>
2025-06-08 17:48:43 +0300

    Add a text image

--- app/src/main/java/com/example/educhat/MainActivity.kt
+++ app/src/main/java/com/example/educhat/MainActivity.kt
@@ -3,27 +3,25 @@ package com.example.educhat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.educhat.ui.theme.EduChatTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             EduChatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ConversationItem(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +29,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ConversationItem(modifier: Modifier) {
+    val image = painterResource(R.drawable.educhat_icon)
+    Box() {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text("HAAAALLO")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EduChatTheme {
-        Greeting("Android")
+        ConversationItem(modifier = Modifier)
     }
 }
