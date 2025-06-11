ec58ff573217d835d2672889ccd3a614e0a21391
Nikos Tsilas <tsilasnikos@gmail.com>
2025-06-11 15:46:19 +0300

    Add colors and theme. Both dark and light scheme as well as make them being previewed.

--- app/src/main/java/com/example/educhat/MainActivity.kt
+++ app/src/main/java/com/example/educhat/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.educhat.ui.theme.EduChatTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,20 +35,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ConversationItem(modifier: Modifier) {
-    val image = painterResource(R.drawable.educhat_icon)
-    Box() {
-        Image(
-            painter = image,
-            contentDescription = null
-        )
-        Text("HAAAALLO")
+
+    Card(modifier = modifier) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val image = painterResource(R.drawable.educhat_icon)
+
+            Image(
+                painter = image,
+                modifier = Modifier
+                    .height(40.dp)
+                    .padding(4.dp),
+                contentDescription = null
+            )
+            Text("HAAAALLO")
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun EduChatAppPreviewLight() {
     EduChatTheme {
-        ConversationItem(modifier = Modifier)
+        ConversationItem(modifier = Modifier.padding(4.dp))
+    }
+}
+
+@Preview
+@Composable
+fun EduChatAppPreviewDark() {
+    EduChatTheme(darkTheme = true) {
+        ConversationItem(modifier = Modifier.padding(4.dp))
     }
 }
